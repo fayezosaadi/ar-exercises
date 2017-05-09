4dc0b35403a179494b9523ab54bd50eb7a736b86
fayezosaadi <fayezosaadi@gmail.com>
2017-05-09 20:36:59 +0000

    Get the 1st, 2nd store, updated the 1st store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.first
+@store2 = Store.find 2
+
+@store1.name = 'Westend'
+@store1.save
+
+puts @store1.name
