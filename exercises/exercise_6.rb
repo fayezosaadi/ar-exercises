05a2991e03105205c631045e9267c4adfdbdbd6c
fayezosaadi <fayezosaadi@gmail.com>
2017-05-09 22:57:12 +0000

    added associations to tables, added  bunch of employees to the table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jeffrey", last_name: "Ling", hourly_rate: 55)
+@store1.employees.create(first_name: "Joel", last_name: "Joel", hourly_rate: 60)
+@store2.employees.create(first_name: "Don", last_name: "Burks", hourly_rate: 10)
+@store2.employees.create(first_name: "Karl", last_name: "Jensen", hourly_rate: 60)
+@store2.employees.create(first_name: "Clint", last_name: "Liang", hourly_rate: 60)
+
+puts Employee.all.count
